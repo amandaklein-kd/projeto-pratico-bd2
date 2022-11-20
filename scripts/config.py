c8807ca695f36837cdffbf557772654dc1b8b79b
Amanda Klein <amandakllein@gmail.com>
2022-11-20 19:48:49 -0300

    Primeiro commit

---
+++ scripts/config.py
@@ -0,0 +1,29 @@
+import psycopg2
+
+
+class DbConfig:
+    # Alterar a variável connection_data de acordo com os dados da máquina em uso.
+    def __init__(
+        self,
+        connection_data="host=localhost dbname=bd2 user=postgres password=postgres", 
+    ):
+        self.connection_data = connection_data
+
+    def alter(self, query, values):
+        try:
+            conn = psycopg2.connect(self.connection_data)
+        except Exception as e:
+            raise e
+
+        try:
+            cur = conn.cursor()
+            cur.execute(query, values)
+            conn.commit()
+            return "sucesso"
+        except Exception as e:
+            conn.rollback()
+            print(e)
+            return e
+        finally:
+            cur.close()
+            conn.close()
